a52e050ba539fa8ddf8901de327f09ea6e961c90
aoizora <mathesis@inbox.ru>
2023-07-02 12:18:02 +0300

    добавляем библиотеку

---
+++ 00-cmake/01-library/lib/lib.cpp
@@ -0,0 +1,11 @@
+#include "lib.h"
+
+#include <iostream>
+
+namespace lib
+{
+    void hello()
+    {
+        std::cout << "Hello, World!" << std::endl;
+    }
+}
---
+++ 00-cmake/01-library/src/main.cpp
@@ -0,0 +1,8 @@
+#include "lib.h"
+
+int main()
+{
+    lib::hello();
+
+    return 0;
+}
