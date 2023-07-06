b4f4323da64e9b775b140ba4735a5195e158f6b3
Aoizora <mathesis@inbox.ru>
2023-07-06 17:36:46 +0300

    Поправил ci/cd

---
+++ build/CMakeFiles/ip-filter.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ip-filter.dir/ip.cpp.o"
+  "CMakeFiles/ip-filter.dir/ip.cpp.o.d"
+  "CMakeFiles/ip-filter.dir/ip_filter.cpp.o"
+  "CMakeFiles/ip-filter.dir/ip_filter.cpp.o.d"
+  "CMakeFiles/ip-filter.dir/misc.cpp.o"
+  "CMakeFiles/ip-filter.dir/misc.cpp.o.d"
+  "ip-filter"
+  "ip-filter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ip-filter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
